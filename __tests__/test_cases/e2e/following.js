bdd8eed407fbcc89a5d122045b2596c47c2774ad
Zhuangda Zhu <zhuangdaz@gmail.com>
2021-04-07 11:06:31 -0400

    Add e2e tests for distribute tweets

--- __tests__/test_cases/e2e/following.js
+++ __tests__/test_cases/e2e/following.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const given = require('../../steps/given')
 const when = require('../../steps/when')
+const chance = require('chance').Chance()
+const retry = require('async-retry')
 
 describe("Given authenticated users, userA and userB", () => {
   let userA, userB, userAsProfile, userBsProfile
@@ -27,6 +29,25 @@ describe("Given authenticated users, userA and userB", () => {
       expect(following).toBe(false)
       expect(followedBy).toBe(true)
     })
+
+    describe("UserB sends a tweet", () => {
+      let tweet
+      const text = chance.string({ length: 16 })
+      beforeAll(async() => {
+        tweet = await when.a_user_calls_tweet(userB, text)
+      })
+
+      it("Should appear in userA's timeline", async() => {
+        await retry(async () => {
+          const { tweets } = await when.a_user_calls_getMyTimeline(userA, 25)
+          expect(tweets).toHaveLength(1)
+          expect(tweets[0].id).toEqual(tweet.id)
+        }, {
+          retries: 3,
+          maxTimeout: 1000
+        })
+      })
+    })
   })
 
   describe("When userB follows back userA", () => {
@@ -44,6 +65,25 @@ describe("Given authenticated users, userA and userB", () => {
       const { following, followedBy } = await when.a_user_calls_getProfile(userB, userAsProfile.screenName)
       expect(following).toBe(true)
       expect(followedBy).toBe(true)
+    })
+
+    describe("UserA sends a tweet", () => {
+      let tweet
+      const text = chance.string({ length: 16 })
+      beforeAll(async() => {
+        tweet = await when.a_user_calls_tweet(userA, text)
+      })
+
+      it("Should appear in userB's timeline", async() => {
+        await retry(async () => {
+          const { tweets } = await when.a_user_calls_getMyTimeline(userB, 25)
+          expect(tweets).toHaveLength(2)
+          expect(tweets[0].id).toEqual(tweet.id)
+        }, {
+          retries: 3,
+          maxTimeout: 1000
+        })
+      })
     })
   })
 })
