9c74f8aafefb083087871d43bd73ef4b9f8320d7
Zhuangda Zhu <zhuangdaz@gmail.com>
2021-04-25 12:26:38 -0400

    Undo retry from then module

--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -3,7 +3,6 @@ const _ = require('lodash')
 const AWS = require('aws-sdk')
 const http = require('axios')
 const fs = require('fs')
-const retry = require('async-retry')
 
 const user_exists_in_UsersTable = async (id) => {
   const DynamoDB = new AWS.DynamoDB.DocumentClient()
@@ -79,26 +78,23 @@ const retweet_exists_in_TweetsTable = async (userId, tweetId) => {
 }
 
 const reply_exists_in_TweetsTable = async (userId, tweetId) => {
-  await retry(async () => {
-    const DynamoDB = new AWS.DynamoDB.DocumentClient()
-    const resp = await DynamoDB.query({
-      TableName: process.env.TWEETS_TABLE,
-      IndexName: 'repliesForTweet',
-      KeyConditionExpression: 'inReplyToTweetId = :tweetId',
-      ExpressionAttributeValues: {
-        ':tweetId': tweetId,
-        ':userId': userId
-      },
-      FilterExpression: 'creator = :userId'
-    }).promise()
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+  const resp = await DynamoDB.query({
+    TableName: process.env.TWEETS_TABLE,
+    IndexName: 'repliesForTweet',
+    KeyConditionExpression: 'inReplyToTweetId = :tweetId',
+    ExpressionAttributeValues: {
+      ':tweetId': tweetId,
+      ':userId': userId
+    },
+    FilterExpression: 'creator = :userId'
+  }).promise()
 
-    const reply = _.get(resp, 'Items.0')
-    expect(reply).toBeTruthy()
-    return reply
-  }, {
-    retries: 5,
-    maxTimeout: 1000
-  })
+  const reply = _.get(resp, 'Items.0')
+
+  expect(reply).toBeTruthy()
+
+  return reply
 }
 
 const retweet_exists_in_RetweetsTable = async (userId, tweetId) => {
